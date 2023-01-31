f7ffc562eedb625d304603e5b1ef522f77c5c914
Łukasz Krenski <sakulk@gmail.com>
2023-01-31 21:47:37 +0100

    header check fix

--- core/src/main/scala/serval/read/mapN.scala
+++ core/src/main/scala/serval/read/mapN.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 serval
+ * Copyright 2023 serval
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
